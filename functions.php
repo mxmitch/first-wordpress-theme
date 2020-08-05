0e0134d2e070b74c71107fbf72878c4fa5468e7f
Mitch Lum <mklum88@gmail.com>
2020-08-05 15:30:10 -0700

    enqueued stylesheets and scripts

--- functions.php
+++ functions.php
@@ -5,3 +5,12 @@ function load_stylesheets() {
 }
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+
+function loadjs() {
+  wp_register_script('customjs', get_template_directory_uri() . 'js/scripts.js', '',1, true);
+  wp_enqueue_script('customjs');
+}
+
+add_action('wp_enqueue_scripts','loadjs');
+
