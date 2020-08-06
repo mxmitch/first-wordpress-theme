f7386c3a1932cf20183705c5e86ca9a40ede0f2b
Mitch Lum <mklum88@gmail.com>
2020-08-06 09:17:13 -0700

    manually downloaded and enqueued jquery

--- functions.php
+++ functions.php
@@ -6,11 +6,19 @@ function load_stylesheets() {
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
+function include_jquery() {
+  wp_deregister_script('jquery');
+  wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery.min.js', '', 1, true);
+  add_action('wp_enqueue_scripts', 'jquery');
+}
+
+add_action('wp_enqueue_scripts', 'include_jquery');
 
 function loadjs() {
-  wp_register_script('customjs', get_template_directory_uri() . 'js/scripts.js', '',1, true);
+  wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '',1, true);
   wp_enqueue_script('customjs');
 }
 
 add_action('wp_enqueue_scripts','loadjs');
+
 
