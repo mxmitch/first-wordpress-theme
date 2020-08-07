aa51c36780f2d17903c41d75ffc3fd978fdc7906
Mitch Lum <mklum88@gmail.com>
2020-08-06 22:57:02 -0700

    added woocommerce and header menu

--- functions.php
+++ functions.php
@@ -1,24 +1,66 @@
 <?php
-function load_stylesheets() {
-  wp_register_style('stylesheet', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
-  wp_enqueue_style('stylesheet');
+function load_stylesheets()
+{
+  wp_register_style(
+    'bootstrap',
+    get_template_directory_uri() . '/css/bootstrap.min.css',
+    [],
+    false,
+    'all'
+  );
+  wp_enqueue_style('bootstrap');
+
+  wp_register_style(
+    'style',
+    get_template_directory_uri() . '/style.css',
+    [],
+    false,
+    'all'
+  );
+  wp_enqueue_style('style');
 }
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
-function include_jquery() {
+function include_jquery()
+{
   wp_deregister_script('jquery');
-  wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery.min.js', '', 1, true);
+  wp_enqueue_script(
+    'jquery',
+    get_template_directory_uri() . '/js/jquery.min.js',
+    '',
+    1,
+    true
+  );
   add_action('wp_enqueue_scripts', 'jquery');
 }
 
 add_action('wp_enqueue_scripts', 'include_jquery');
 
-function loadjs() {
-  wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '',1, true);
+function loadjs()
+{
+  wp_register_script(
+    'customjs',
+    get_template_directory_uri() . '/js/scripts.js',
+    '',
+    1,
+    true
+  );
   wp_enqueue_script('customjs');
 }
 
-add_action('wp_enqueue_scripts','loadjs');
+add_action('wp_enqueue_scripts', 'loadjs');
 
+function customtheme_add_woocommerce_support()
+{
+  add_theme_support('woocommerce');
+}
 
+add_action('after_setup_theme', 'customtheme_add_woocommerce_support');
+
+add_theme_support('menus');
+
+register_nav_menus([
+  'top-menu' => __('Top Menu', 'theme'),
+  'footer-menu' => __('Footer Menu', 'theme'),
+]);
--- header.php
+++ header.php
@@ -3,7 +3,18 @@
 
   <head>
 
-    <?php wp_head();?>
+    <?php wp_head(); ?>
   </head>
 
-<body <?php body_class();?>>
+<body <?php body_class(); ?>>
+
+<header class="sticky-top">
+  <div class="container">
+    <?php wp_nav_menu([
+      'theme_location' => 'top-menu',
+      'menu_class' => 'navigation',
+    ]); ?>
+  </div>
+</header>
+
+<div class="container">
