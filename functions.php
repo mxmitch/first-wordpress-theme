4665ddf93e34d196821e12c55805e83aa632949a
Mitch Lum <mklum88@gmail.com>
2020-08-07 00:24:54 -0700

    added woocommerce pages

--- functions.php
+++ functions.php
@@ -60,7 +60,36 @@ add_action('after_setup_theme', 'customtheme_add_woocommerce_support');
 
 add_theme_support('menus');
 
+add_theme_support('post-thumbnails');
+
 register_nav_menus([
   'top-menu' => __('Top Menu', 'theme'),
   'footer-menu' => __('Footer Menu', 'theme'),
 ]);
+
+add_image_size('smallest', 300, 300, true);
+add_image_size('largest', 800, 800, true);
+
+remove_action(
+  'woocommerce_before_main_content',
+  'woocommerce_output_content_wrapper',
+  10
+);
+remove_action(
+  'woocommerce_after_main_content',
+  'woocommerce_output_content_wrapper_end',
+  10
+);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start()
+{
+  echo '<section id="main">';
+}
+
+function my_theme_wrapper_end()
+{
+  echo '</section>';
+}
--- header.php
+++ header.php
@@ -9,7 +9,8 @@
 <body <?php body_class(); ?>>
 
 <header class="sticky-top">
-  <div class="container">
+  <div class="container d-flex align-items-center justify-content-between h-100">
+      <a href="/custom-template/"><h1>Nuts 'n Things</h1></a>
     <?php wp_nav_menu([
       'theme_location' => 'top-menu',
       'menu_class' => 'navigation',
